1bca0b352faf22d04e29da129db832eae9e9ae9c
sb2676 <sb2676@g.rit.edu>
2025-09-11 11:48:23 +0200

    saving

---
+++ w01c2-demo-materialize-starter/w01c2-demo-materialize-starter/js/controller/cupcakeController.js
@@ -0,0 +1,5 @@
+//controller is filled with handelers
+
+//do constructor then handle handelers
+
+//in event handeler it should read input, update the model and update the view
---
+++ w01c2-demo-materialize-starter/w01c2-demo-materialize-starter/js/view/cupcakeView.js
@@ -0,0 +1,10 @@
+class cupcakeView{
+    constructor(){
+        this.breadImput = document.querySelector("#input-bread img");
+        this.frostinput = document.querySelector('#input-frosting img');");
+        this.decorinput = document.querySelector("#div-animal img");
+    }
+    renderCupcake(bread, frosting, decor){
+        let imgSrc = "media/${bread}-cupcake.png";
+    }
+}
