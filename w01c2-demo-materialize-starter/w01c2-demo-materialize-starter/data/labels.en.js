66ec453bcabb3ae2ce7d18adc1e75db3c8dd8b68
sb2676 <sb2676@g.rit.edu>
2025-09-18 11:25:39 +0200

    saving

---
+++ w01c2-demo-materialize-starter/w01c2-demo-materialize-starter/data/labels.en.js
@@ -0,0 +1,7 @@
+export const labels = {
+    product: "Sugar Cup",
+    productPlural: "Sugar Cups",
+    productTagline: "Create your Sugar Cup!",
+    productLegend: "Sugar Cup Option",
+    productPreviewLabel: "Your Sugar Cup Preview"
+};
--- w01c2-demo-materialize-starter/w01c2-demo-materialize-starter/js/model/cupcakeModel.js
+++ w01c2-demo-materialize-starter/w01c2-demo-materialize-starter/js/model/cupcakeModel.js
@@ -1,7 +1,17 @@
 export class CupcakeModel{
-    constructor(){
+    #selects = null;//pointer to the data
+
+    //make method to get properties andvalues and options
+
+
+    constructor(selects){
+        this.#selects=selects;
+        //
         this.bread = "";//for the bread
         this.icing = "";//for the icing
         this.decor = "";//for the decor
+        //
+        let properties = Object.keys(this.#selects);
+       //Define 
     }
 }
